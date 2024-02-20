f84eb0b05698977ecbb34e3f43e945d7c6a30e44
Dimitar Fenerski <dfenerski@gmail.com>
2024-02-20 16:04:09 +0200

    feat: disable some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,25 @@
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		es6: true,
-		node: true,
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-		sourceType: "module",
-	},
-	plugins: ["@typescript-eslint"],
-	ignorePatterns: [".eslintrc.js"],
+    root: true,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/ban-types': 'off',
+    },
 };
