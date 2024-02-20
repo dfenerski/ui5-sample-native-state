d697121798d065bc472961912baf5dec9b6ccd2a
Dimitar Fenerski <dfenerski@gmail.com>
2024-02-20 16:05:47 +0200

    feat: add layout setter to app model

--- webapp/model/app/AppState.service.ts
+++ webapp/model/app/AppState.service.ts
@@ -1,7 +1,11 @@
 import { StateService } from '../State.service';
 import { App } from './App';
 
-class AppStateService extends StateService<App> {}
+class AppStateService extends StateService<App> {
+    public setLayout(layout: App['layout']) {
+        this._model.setProperty('/layout', layout);
+    }
+}
 
 const appModel = new AppStateService('app', new App());
 
