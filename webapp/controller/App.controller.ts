32871a987eab5b2d0a5eca96ee9c4144254de26d
Dimitar Fenerski <dfenerski@gmail.com>
2024-02-20 16:03:15 +0200

    feat: add theme switcher, use anonymous imports for models

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,16 +1,20 @@
-import { TaskStateService } from '../model/task/TaskState.service';
+import Theming from 'sap/ui/core/Theming';
+import '../model/app/AppState.service';
+import '../model/task/TaskState.service';
 import BaseController from './BaseController';
 
 /**
  * @namespace com.github.dfenerski.native_state.controller
  */
 export default class App extends BaseController {
-    private readonly taskModel = TaskStateService;
-
     public onInit(): void {
         // Apply content density mode to root view
         this.getView().addStyleClass(
             this.getOwnerComponent().getContentDensityClass(),
         );
+    }
+
+    public handleThemeChange(theme: 'sap_horizon' | 'sap_horizon_dark'): void {
+        Theming.setTheme(theme);
     }
 }
