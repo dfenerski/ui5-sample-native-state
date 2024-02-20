81f463a5a5675b6c40426c739407f921250e51ce
Dimitar Fenerski <dfenerski@gmail.com>
2024-02-20 16:06:26 +0200

    feat: add task model setters

--- webapp/model/task/TaskState.service.ts
+++ webapp/model/task/TaskState.service.ts
@@ -1,7 +1,46 @@
 import { StateService } from '../State.service';
-import { Task } from './Task';
+import { Task, TaskItem } from './Task';
 
-class TaskStateService extends StateService<Task> {}
+class TaskStateService extends StateService<Task> {
+    public setTaskTitle(id: TaskItem['id'], title: TaskItem['title']) {
+        const taskIndex = this.state.items.findIndex((task) => task.id === id);
+        this._model.setProperty(`/items/${taskIndex}/title`, title);
+    }
+
+    public setTaskDescription(
+        id: TaskItem['id'],
+        description: TaskItem['description'],
+    ) {
+        const taskIndex = this.state.items.findIndex((task) => task.id === id);
+        this._model.setProperty(`/items/${taskIndex}/description`, description);
+    }
+
+    public setTaskStatus(id: TaskItem['id'], status: TaskItem['status']) {
+        const taskIndex = this.state.items.findIndex((task) => task.id === id);
+        this._model.setProperty(`/items/${taskIndex}/status`, status);
+    }
+
+    public setTaskPriority(id: TaskItem['id'], priority: TaskItem['priority']) {
+        const taskIndex = this.state.items.findIndex((task) => task.id === id);
+        this._data.items[taskIndex].priority = priority;
+        this._model.updateBindings(true);
+    }
+
+    public setSelectedTask(task: TaskItem | null) {
+        this._model.setProperty('/selectedTask', task);
+    }
+
+    public addTask(task: TaskItem) {
+        this._data.items.push(task);
+        this._model.updateBindings(true);
+    }
+
+    public removeTask(id: TaskItem['id']) {
+        const taskIndex = this.state.items.findIndex((task) => task.id === id);
+        this._data.items.splice(taskIndex, 1);
+        this._model.updateBindings(true);
+    }
+}
 
 const taskModel = new TaskStateService(
     'task',
@@ -11,21 +50,21 @@ const taskModel = new TaskStateService(
                 id: '1',
                 title: 'Task 1',
                 description: 'Description 1',
-                priority: 'low',
+                priority: 1,
                 status: 'open',
             },
             {
                 id: '2',
                 title: 'Task 2',
                 description: 'Description 2',
-                priority: 'medium',
+                priority: 2,
                 status: 'inProgress',
             },
             {
                 id: '3',
                 title: 'Task 3',
                 description: 'Description 3',
-                priority: 'high',
+                priority: 3,
                 status: 'open',
             },
         ],
